283ec40509dc6f73bf3191b84f848d380290f600
Luiz S.S. Baglie <luizssb.biz@gmail.com>
2021-03-27 15:51:27 -0300

    Provided some styling for the product items

--- app/src/main/java/com/luizssb/adidas/confirmed/view/viewholder/ProductViewHolder.kt
+++ app/src/main/java/com/luizssb/adidas/confirmed/view/viewholder/ProductViewHolder.kt
@@ -3,6 +3,8 @@ package com.luizssb.adidas.confirmed.view.viewholder
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.luizssb.adidas.confirmed.R
 import com.luizssb.adidas.confirmed.databinding.ItemProductBinding
 import com.luizssb.adidas.confirmed.dto.Product
 
@@ -10,14 +12,17 @@ class ProductViewHolder(private val layout: ItemProductBinding) : RecyclerView.V
     fun bind(item: Product?) {
         val fixdItem = item ?: PLACEHOLDER_PRODUCT
         layout.textName.text = fixdItem.name
+        layout.textDescription.text = fixdItem.description
+        layout.textPrice.text = "R$"+fixdItem.price.toString()
 
-//        if (item == null) {
-//            layout.imageAvatar.setImageDrawable(null)
-//        } else {
-//            Glide.with(layout.root.context)
-//                    .load(item.photo)
-//                    .into(layout.imageAvatar)
-//        }
+        if (item == null) {
+            layout.image.setImageDrawable(null)
+        } else {
+            Glide.with(layout.root.context)
+                .load(item.imgUrl)
+                .placeholder(R.mipmap.ic_launcher_desaturated)
+                .into(layout.image)
+        }
     }
 
     companion object {
