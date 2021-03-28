4c43dd7ef027e39761758e694875af5818dc1c7b
Luiz S.S. Baglie <luizssb.biz@gmail.com>
2021-03-27 21:14:30 -0300

    Basic setup of navigation. Gotta refactor the product list.

--- app/src/main/java/com/luizssb/adidas/confirmed/view/MainActivity.kt
+++ app/src/main/java/com/luizssb/adidas/confirmed/view/MainActivity.kt
@@ -2,17 +2,14 @@ package com.luizssb.adidas.confirmed.view
 
 import android.os.Bundle
 import android.view.Menu
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
-import androidx.lifecycle.Observer
-import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.NavHostFragment
+import com.luizssb.adidas.confirmed.ProductListFragmentDirections
 import com.luizssb.adidas.confirmed.R
 import com.luizssb.adidas.confirmed.databinding.ActivityMainBinding
 import com.luizssb.adidas.confirmed.view.adapter.ProductsAdapter
 import com.luizssb.adidas.confirmed.viewmodel.product.ProductListViewModel
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
@@ -26,27 +23,30 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layout.root)
-        setSupportActionBar(layout.toolbar)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_navhost) as NavHostFragment
+        val navController = navHostFragment.navController
+        val action = ProductListFragmentDirections.actionProductListFragmentToProductFragment()
+//        setContentView(layout.root)
+//        setSupportActionBar(layout.toolbar)
 
-        with(layout) {
-            refresh.setOnRefreshListener {
-                viewModel.handleIntent(ProductListViewModel.Intent.Refresh)
-            }
-            list.adapter = itemAdapter
-        }
-
-        lifecycleScope.launch {
-            itemAdapter.loadStateFlow.collectLatest {
-                viewModel.handleIntent(ProductListViewModel.Intent.ChangeLoadState(it))
-            }
-        }
-
-        viewModel.let {
-            it.state.observe(this, Observer(this::render))
-            it.effects.observe(this, Observer(this::render))
-            it.startOrResume()
-        }
+//        with(layout) {
+//            refresh.setOnRefreshListener {
+//                viewModel.handleIntent(ProductListViewModel.Intent.Refresh)
+//            }
+//            list.adapter = itemAdapter
+//        }
+//
+//        lifecycleScope.launch {
+//            itemAdapter.loadStateFlow.collectLatest {
+//                viewModel.handleIntent(ProductListViewModel.Intent.ChangeLoadState(it))
+//            }
+//        }
+//
+//        viewModel.let {
+//            it.state.observe(this, Observer(this::render))
+//            it.effects.observe(this, Observer(this::render))
+//            it.startOrResume()
+//        }
     }
 
     // luizssb: the challenge's description described the app as being composed by two pages, so I am
@@ -77,18 +77,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun render(state: ProductListViewModel.State) {
-        layout.refresh.isRefreshing = state.loadingRefresh
-        lifecycleScope.launch {
-            itemAdapter.submitData(state.products)
-        }
+//        layout.refresh.isRefreshing = state.loadingRefresh
+//        lifecycleScope.launch {
+//            itemAdapter.submitData(state.products)
+//        }
     }
 
     private fun render(effect: ProductListViewModel.Effect) {
-        when(effect) {
-            ProductListViewModel.Effect.Refresh -> itemAdapter.refresh()
-            is ProductListViewModel.Effect.ShowError ->
-                // TODO luizssb: replace with snackbar
-                Toast.makeText(this, effect.error.message, Toast.LENGTH_SHORT).show()
-        }
+//        when(effect) {
+//            ProductListViewModel.Effect.Refresh -> itemAdapter.refresh()
+//            is ProductListViewModel.Effect.ShowError ->
+//                // TODO luizssb: replace with snackbar
+//                Toast.makeText(this, effect.error.message, Toast.LENGTH_SHORT).show()
+//        }
     }
 }
