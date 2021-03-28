6915693caf9d27c042e9aa48dc6908607ca3519f
Luiz S.S. Baglie <luizssb.biz@gmail.com>
2021-03-28 09:15:01 -0300

    Fixed how the product service layer got info on one single product.

--- app/src/main/java/com/luizssb/adidas/confirmed/service/product/ProductServiceImpl.kt
+++ app/src/main/java/com/luizssb/adidas/confirmed/service/product/ProductServiceImpl.kt
@@ -15,32 +15,18 @@ class ProductServiceImpl(private val api: RetrofitProductRESTAPI) : ProductServi
     override suspend fun getProducts(searchQuery: String?, pageRef: PageRef): PaginationResult<Product> =
             withContext(Dispatchers.IO) {
                 val products = api.getProducts().await()
-
-                        // luizssb: there is an is an entry in the sample database that, for some
-                        // reason, has all null data. I'm assumming this is not on purpose, therefore
-                        // to avoid having to implement some extra structures to handle this single
-                        // problematic case, I just cutting it off right here.
-                        .filter {
-                            @Suppress("SENSELESS_COMPARISON")
-                            it.id !== null
-                        }
-
+                        .filter { it.isValid }
                         // luizssb: since we are faking filtering, may as well fake pagination.
                         .run { subList(pageRef.skip, min(pageRef.upperBound, size)) }
+                        .map { it.fakeData()}
 
-                        // luizssb: also faking prices and currency, which come as nil
-                        .map { it.copy(
-                                price = (it.id.hashCode() / 1e6).toFloat(),
-                                currency = "EUR"
-                        ) }
-
-                // luizssb: simple check to avoid having to filter when there's no filter.
+                // luizssb: simple check to avoid having to filter when there's no search query.
                 val filtered = searchQuery?.takeIf { it.isNotBlank() }
                         ?.run { toLowerCase(Locale.getDefault()) }
                         ?.let { query ->
                             products.filter {
-                                // luizssb: filters on the ids, as all the products in the sample database
-                                // have the same name.
+                                // luizssb: filters on the ids, as all the products in the sample
+                                // database have the same name.
                                 it.id.toLowerCase(Locale.getDefault()).contains(query)
                             }
                 } ?:
@@ -51,10 +37,28 @@ class ProductServiceImpl(private val api: RetrofitProductRESTAPI) : ProductServi
 
     override suspend fun getProduct(productId: String): Product? = withContext(Dispatchers.IO) {
         try {
-            api.getProduct(productId).await()
+            api.getProduct(productId)
+                    .await()
+                    .takeIf { it.isValid }
+                    ?.run { fakeData() }
         } catch (ex: HttpException) {
             if (ex.code() == 500) null
                 else throw ex
         }
+    }
+
+    companion object {
+        // luizssb: also faking prices and currency, which come nil
+        private fun Product.fakeData() = copy(
+                price = (id.hashCode() / 1e6).toFloat(),
+                currency = "EUR"
+        )
+
+        // luizssb: there is an is an entry in the sample database that, for some
+        // reason, has all null data. I'm assumming this is not on purpose, therefore
+        // to avoid having to implement some extra structures to handle this single
+        // problematic case, I'm just cutting it off right here.
+        @Suppress("SENSELESS_COMPARISON")
+        private val Product.isValid get() = id != null
     }
 }
